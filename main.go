ed69a0522be6e1bd46001aec0ad66d7acb81b6b4
Jordan Walsh <jordanwalsh@Jordans-Mac-Studio.local>
2024-03-01 21:33:28 -0500

    progress

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "github.com/charmbracelet/bubbles/list"
+
 type status int
 
 const (
@@ -24,4 +26,20 @@ func (t Task) Title() string {
 
 func (t Task) Description() string {
 	return t.description
+}
+
+type Model struct {
+	list list.Model
+	err  error
+}
+
+func (m *Model) initList() {
+	m.list = list.New([]list.item{}, list.NewDefaultDelegate())
+	m.list.Title = "Options"
+	m.list.SetItems([]list.item{
+		Task{status: todo, title: "Invade Taiwan", description: "You show great spirit. Nom Nom the chips"},
+		Task{status: inProgress, title: "Invade Russia", description: "Borscht and conquest"},
+		Task{status: done, title: "Invade Japan", description: "All their neko are belong to us."},
+	})
+
 }
