8fec3d1917969fc54892a2a3e86c71e5b2e241dd
Jordan Walsh <jordanwalsh@Jordans-Mac-Studio.local>
2024-03-01 21:43:47 -0500

    pt 1

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "github.com/charmbracelet/bubbles/list"
+import (
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+)
 
 type status int
 
@@ -33,13 +39,49 @@ type Model struct {
 	err  error
 }
 
-func (m *Model) initList() {
-	m.list = list.New([]list.item{}, list.NewDefaultDelegate())
+func New() *Model {
+	return &Model{}
+}
+
+func (m *Model) initList(width, height int) {
+	m.list = list.New([]list.Item{}, list.NewDefaultDelegate(), width, height)
 	m.list.Title = "Options"
-	m.list.SetItems([]list.item{
+	m.list.SetItems([]list.Item{
 		Task{status: todo, title: "Invade Taiwan", description: "You show great spirit. Nom Nom the chips"},
 		Task{status: inProgress, title: "Invade Russia", description: "Borscht and conquest"},
 		Task{status: done, title: "Invade Japan", description: "All their neko are belong to us."},
 	})
 
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "esc", "ctrl+c":
+			return m, tea.Quit
+		}
+	case tea.WindowSizeMsg:
+		m.initList(msg.Width, msg.Height)
+	}
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+}
+
+func (m Model) View() string {
+	return m.list.View()
+}
+
+func main() {
+	m := New()
+	p := tea.NewProgram(m)
+	if err := p.Start(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
