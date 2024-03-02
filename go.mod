31f89263a2c30a75720efc7a8bd8a46275867402
Jordan Walsh <jordanwalsh@Jordans-Mac-Studio.local>
2024-03-01 21:25:29 -0500

    setup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module bt-kanban
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+type status int
+
+const (
+	todo status = iota
+	inProgress
+	done
+)
+
+type Task struct {
+	status      status
+	title       string
+	description string
+}
+
+func (t Task) FilterValue() string {
+	return t.title
+}
+
+func (t Task) Title() string {
+	return t.title
+}
+
+func (t Task) Description() string {
+	return t.description
+}
